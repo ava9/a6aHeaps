41233da1956c5a45f43251ce09c32f8df101fdd4
mathkid95 <shankar.athi07@gmail.com>
2014-11-22 15:41:13 -0500

    updatePriority function completed
    2 private helper functions, specifically swap and getParent, have also
    been added.

--- src/MyHeap.java
+++ src/MyHeap.java
@@ -4,13 +4,13 @@ import java.util.HashMap;
 /** An instance is a heap of elements of type T. */
 public class MyHeap<T> implements MinHeap<T> {
     
-	ArrayList<HeapEntry> heap = new ArrayList<HeapEntry>();
-	HashMap<T, HeapEntry> heapMap = new HashMap<T, HeapEntry>();
+	ArrayList<HeapEntry> heap;
+	HashMap<MyHeap<T>, HeapEntry> heapMap;
 	
     /** Constructor: an empty heap. */
     public MyHeap() { 
-    	
-    	
+    	heap = new ArrayList<HeapEntry>();
+    	heapMap = new HashMap<MyHeap<T>, HeapEntry>();
     }
 
     /** Return a representation of this heap. */
@@ -34,7 +34,21 @@ public class MyHeap<T> implements MinHeap<T> {
 
     /** Change the priority of item to p. */
     public @Override void updatePriority(T item, double p) {
+        HeapEntry entry = (HeapEntry) item;
         
+        if(entry.priority < p){
+        	throw new IllegalArgumentException();
+        }
+        
+        entry.priority = p;
+        
+        HeapEntry parent = heap.get(getParent(entry.index));
+        
+        if(parent.compareTo(entry) > 0){
+        	 swap(parent, entry);
+        }
+        
+       
     }
 
     /** Return the size of this heap. */
@@ -49,7 +63,39 @@ public class MyHeap<T> implements MinHeap<T> {
     	return heap.size() == 0;
     }
     
-    public static class HeapEntry{
+    //Return parent index of a node in the heap
+    private int getParent(int index) {
+		if (index < 0 || index >= heap.size()){
+			throw new IllegalArgumentException();
+		}
+			
+		return (index - 1) / 2;
+	}
+    
+    //Swap two nodes in the heap
+    private void swap(HeapEntry entry1, HeapEntry entry2) {
+    	
+  	  if (entry1 == null && entry2 == null){
+  		
+  		  throw new IllegalArgumentException();
+  	  }
+  	    
+  	  int index1 = entry1.index;
+  	  int index2 = entry2.index;
+  	  
+  	 if (index1 < 0 || index1 > heap.size() || index2 < 0 || index2 > heap.size()){
+  		 throw new IllegalArgumentException();
+  	 }
+ 	   
+  	 entry1.index = index2;
+  	 entry2.index = index1;
+  	 
+  	 heap.set(entry1.index, entry2);
+  	 heap.set(entry2.index, entry1);
+    
+   }
+    
+    public static class HeapEntry implements Comparable<HeapEntry>{
     	
     	public int index;
     	public double priority;
@@ -57,6 +103,13 @@ public class MyHeap<T> implements MinHeap<T> {
     	public HeapEntry(int i, double p){
     		this.index = i;
     		this.priority = p;
+    	}
+    	
+    	@Override
+    	public int compareTo(HeapEntry entry){
+    		
+    		return Double.compare(priority, entry.priority);
+    		
     	}
     }
 }
