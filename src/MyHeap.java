d56f1926bf13712782d4ca129be108dba473b6da
Xprt 200 <spyguy101@gmail.com>
2014-11-24 18:29:17 -0500

    debugged

--- src/MyHeap.java
+++ src/MyHeap.java
@@ -16,15 +16,30 @@ public class MyHeap<T> implements MinHeap<T> {
 
     /** Return a representation of this heap. */
     public @Override String toString(){
-        //TODO  
-        return "";
+        String s = "[";
+        //	 * [item1:priority1, item2:priority2, ..., itemN:priorityN]
+    	for (int i=0; i < heapMap.size(); i++){
+    		if (i == heapMap.size()-1){
+    			s = s + heap.get(i) + ":" + heapMap.get(heap.get(i)).priority;
+    		}
+    		else{
+    			s = s + heap.get(i) + ":" + heapMap.get(heap.get(i)).priority + ", ";
+    		}
+        }
+    	s = s + "]";
+        return s;
     }
 
     /** Remove and return the min value in this heap.
      * Precondition: The heap is not empty. */
     public @Override T poll() {
-        //TODO 
-        return null;
+        if(heap.size() == 0){
+        	return null;
+        }
+        T temp = heap.get(0);
+        bubbleUp(heap.size()-1);
+        bubbleDown(0);
+        return temp;
     }
 
     /** Add item with priority p to this heap.
@@ -41,23 +56,16 @@ public class MyHeap<T> implements MinHeap<T> {
 
     /** Change the priority of item to p. */
     public @Override void updatePriority(T item, double p) {
-    	
-    	/*
-        HeapEntry entry = (HeapEntry) item;
-        
+        HeapEntry entry = heapMap.get(item);
         if(entry.priority < p){
         	throw new IllegalArgumentException();
         }
-        
         entry.priority = p;
-        
-        HeapEntry parent = heap.get(getParent(entry.index));
-        
+        HeapEntry parent = heapMap.get(heap.get(entry.index));
         if(parent.compareTo(entry) > 0){
-        	 swap(parent, entry);
+        	 swap(heap.get(parent.index),heap.get(entry.index));
+        	 bubbleDown(parent.index);
         }
-        */
-       
     }
 
     /** Return the size of this heap. */
@@ -80,27 +88,53 @@ public class MyHeap<T> implements MinHeap<T> {
 			
 		return (index - 1) / 2;
 	}
+    /** Bubble element k up the tree */
+    private void bubbleUp(int k){
+    	int p = this.getParent(k);
+    	while ((k > 0) && (heapMap.get(heap.get(k)).compareTo(heapMap.get(heap.get(p))) < 0)){
+    		swap(heap.get(k), heap.get(p));
+    		//k = p;
+    		//p = (k-1)/2;
+    	}
+    }
+    
+    /** Bubble the root down to its heap position. */
+    private void bubbleDown(int k){
+    	k = 0;
+    	int c = 2*k+2;
+    	if ((c > heap.size()-1) || (heapMap.get(heap.get(c-1)).compareTo(heapMap.get(heap.get(c))) < 0)){
+    		c--;
+    	}
+    	while ((c < heap.size()) && ((heapMap.get(heap.get(k)).compareTo(heapMap.get(heap.get(c))) > 0))){
+    		swap(heap.get(k), heap.get(c));
+    		//k = c;
+    		//c = 2*k+2;
+    		if ((c > heap.size()-1) || (heapMap.get(heap.get(c-1)).compareTo(heapMap.get(heap.get(c))) < 0)){
+        		c--;
+        	}
+    	}
+    }
     
     //Swap two nodes in the heap
-    private void swap(HeapEntry entry1, HeapEntry entry2) {
+    private void swap(T entry1, T entry2) {
     	
   	  if (entry1 == null && entry2 == null){
   		
   		  throw new IllegalArgumentException();
   	  }
   	    
-  	  int index1 = entry1.index;
-  	  int index2 = entry2.index;
+  	  int index1 = heapMap.get(entry1).index;
+  	  int index2 = heapMap.get(entry2).index;
   	  
   	 if (index1 < 0 || index1 > heap.size() || index2 < 0 || index2 > heap.size()){
   		 throw new IllegalArgumentException();
   	 }
  	   
-  	 entry1.index = index2;
-  	 entry2.index = index1;
+  	 heapMap.get(entry1).index = heapMap.get(entry2).index;
+  	 heapMap.get(entry2).index = heapMap.get(entry1).index;
   	 
-  	 //heap.set(entry1.index, entry2);
-  	 //heap.set(entry2.index, entry1);
+  	 heap.set(heapMap.get(entry1).index, entry2);
+  	 heap.set(heapMap.get(entry2).index, entry1);
     
    }
     
